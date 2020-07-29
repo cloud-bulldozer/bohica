5efcec2c7cec7d88c6d1255e5eb144cdef99960a
Ben England <bengland@redhat.com>
2020-07-29 15:56:59 -0400

    make service port number configurable with env.var. also remove exception handler for file-not-found add correct check for /proc filesystem

--- kernel_cache_dropper/kernel-cache-drop-websvc.py
+++ kernel_cache_dropper/kernel-cache-drop-websvc.py
@@ -1,14 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import subprocess
 import logging
 import cherrypy
 import atexit
 import os
+import sys
 
 logging.basicConfig(filename='/tmp/dropcache.log', level=logging.DEBUG)
 logger = logging.getLogger('dropcache')
 logger.setLevel(logging.INFO)
+
+# sanity check to make sure our k8s volume is actually
+# hooked into the /proc filesystem
+
+if not os.access('/proc_sys_vm/dirty_ratio', os.R_OK):
+    logger.error('No access to /proc filesystem, exiting')
+    sys.exit(1)
+
+svcPortNum=9435
+portnumstr = os.getenv('KCACHE_DROP_PORT_NUM')
+if portnumstr != None:
+    svcPortNum = int(portnumstr)
 
 def flush_log():
     logging.shutdown()
@@ -24,21 +37,18 @@ class DropKernelCache(object):
     @cherrypy.expose
     def DropKernelCache(self):
         logger.info('asked for cache drop')
-        try:
-            os.sync()
-            logger.info('completed sync call')
-            with open('/proc_sys_vm/drop_caches','a') as dcf:
-                dcf.write('3\n')
-            logger.info('completed cache drop')
-        except FileNotFoundError:
-                logger.error('/proc_sys_vm/drop_caches not found')
+        os.sync()
+        logger.info('completed sync call')
+        with open('/proc_sys_vm/drop_caches','a') as dcf:
+            dcf.write('3\n')
+        logger.info('completed cache drop')
         return 'SUCCESS'
 
 if __name__ == '__main__':
     config = { 
         'global': {
             'server.socket_host': '0.0.0.0' ,
-            'server.socket_port': 9435,
+            'server.socket_port': svcPortNum,
         },
     }
     cherrypy.config.update(config)
